360a33d8b65a479e9b4de5ef4fc0b7e38a4b7ffa
Keerthana Take <keerthanasrinivas732@gmail.com>
2025-10-15 16:11:55 -0400

    TemperatureConverter

--- app/src/main/java/com/sample/kotlinquizchallenge/TemperatureConverter.kt
+++ app/src/main/java/com/sample/kotlinquizchallenge/TemperatureConverter.kt
@@ -11,7 +11,14 @@ package com.sample.kotlinquizchallenge
 * 25.0 → 77.0
 * 98.6 → 37.0
 * */
+fun convertTemperature(value: Double, converter: (Double) -> Double): Double {
+    return converter(value)
+}
+
+val celsiusToFahrenheit: (Double) -> Double = { c -> (c * 9 / 5) + 32 }
+val fahrenheitToCelsius: (Double) -> Double = { f -> (f - 32) * 5 / 9 }
 
 fun main() {
-
+    println("25.0°C → ${convertTemperature(25.0, celsiusToFahrenheit)}°F")
+    println("98.6°F → ${convertTemperature(98.6, fahrenheitToCelsius)}°C")
 }
