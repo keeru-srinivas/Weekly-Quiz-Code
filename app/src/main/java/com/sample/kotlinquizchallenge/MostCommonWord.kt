a41e2430874571e148709e60e1b1eb3bc80325ee
Keerthana Take <keerthanasrinivas732@gmail.com>
2025-10-15 15:49:03 -0400

    MostCommonWord

--- app/src/main/java/com/sample/kotlinquizchallenge/MostCommonWord.kt
+++ app/src/main/java/com/sample/kotlinquizchallenge/MostCommonWord.kt
@@ -11,6 +11,20 @@ package com.sample.kotlinquizchallenge
 *
 * */
 
-fun main() {
+fun findMostCommonWord(text: String):String {
+    val stopWords = setOf("the", "is", "a", "an", "and", "of", "to", "in", "on", "for", "with")
+    val words = text
+        .lowercase()
+        .split(" ", ",", ".", "!", "?", ";", ":")
+        .filter { it.isNotBlank() && it !in stopWords }
+    if (words.isEmpty()) return "No valid words"
+    val wordCount = words.groupingBy { it }.eachCount()
+    val mostCommon = wordCount.maxByOrNull{it.value}?.key?: "No words found"
 
+    return mostCommon
+
+}
+fun main() {
+    val sentence = "The sun shines and the sky is blue and clear"
+    println("\"$sentence\" → \"${findMostCommonWord(sentence)}\"")
 }
