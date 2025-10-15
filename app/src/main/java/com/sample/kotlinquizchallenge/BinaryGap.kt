cf811919131dd5fc8b31ad3955ad6c796584875c
Keerthana Take <keerthanasrinivas732@gmail.com>
2025-10-15 14:57:22 -0400

    BinaryGap Solution

--- app/src/main/java/com/sample/kotlinquizchallenge/BinaryGap.kt
+++ app/src/main/java/com/sample/kotlinquizchallenge/BinaryGap.kt
@@ -14,6 +14,35 @@ package com.sample.kotlinquizchallenge
 * 32 → 0 (no gap surrounded by ones)
 * */
 
-fun main() {
+fun findBinaryGap(n:Int): Int {
+    val binary = Integer.toBinaryString(n)
+    var maxGap = 0
+    var currentGap = 0
+    var counting = false
 
+    for (char in binary){
+        when (char){
+            '1'-> {
+                if (counting) {
+                    maxGap = maxOf(maxGap, currentGap)
+                }
+                counting = true
+                currentGap = 0
+            }
+            '0'-> {
+                if (counting) {
+                    currentGap++
+                }
+            }
+        }
+    }
+    return maxGap
 }
+
+fun main() {
+    println("9 -> ${findBinaryGap(9)}")
+    println("529 -> ${findBinaryGap(529)}")
+    println("32 → ${findBinaryGap(32)}")
+}
+
+
